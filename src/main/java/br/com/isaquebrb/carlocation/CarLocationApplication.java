3ec8409fa00dd17b95c00e19044cfa8d1ee16995
Isaque Bernardes <isaque.ribeiro.bernardes@gmail.com>
2022-08-06 16:42:49 -0300

    Project creation

---
+++ src/main/java/br/com/isaquebrb/carlocation/CarLocationApplication.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.carlocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isaquebrb/carlocation/CarLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.carlocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
